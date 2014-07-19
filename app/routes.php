f17a038cbc248eee31524d81db35dc760e01b761
Joseph Le <josephle@me.com>
2014-07-18 23:03:49 -0700

    Customized authentication routes

--- app/routes.php
+++ app/routes.php
@@ -16,14 +16,14 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-// Confide routes
-Route::get( 'user/create',                 'UserController@create');
-Route::post('user',                        'UserController@store');
-Route::get( 'user/login',                  'UserController@login');
-Route::post('user/login',                  'UserController@do_login');
-Route::get( 'user/confirm/{code}',         'UserController@confirm');
-Route::get( 'user/forgot_password',        'UserController@forgot_password');
-Route::post('user/forgot_password',        'UserController@do_forgot_password');
-Route::get( 'user/reset_password/{token}', 'UserController@reset_password');
-Route::post('user/reset_password',         'UserController@do_reset_password');
-Route::get( 'user/logout',                 'UserController@logout');
+/* Authentication Routes */
+Route::get( 'register',                     'UserController@create');
+Route::post('register',                     'UserController@store');
+Route::get( 'register/confirm/{code}',      'UserController@confirm');
+Route::get( 'login',                        'UserController@login');
+Route::post('login',                        'UserController@do_login');
+Route::get( 'login/forgot_password',        'UserController@forgot_password');
+Route::post('login/forgot_password',        'UserController@do_forgot_password');
+Route::get( 'login/reset_password/{token}', 'UserController@reset_password');
+Route::post('login/reset_password',         'UserController@do_reset_password');
+Route::get( 'logout',                       'UserController@logout');
