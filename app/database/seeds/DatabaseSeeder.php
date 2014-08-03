09bdfc5e05b70f4258fa23279e08b11b50daea1e
Joseph Le <josephle@me.com>
2014-08-02 19:30:02 -0700

    #15244840113834: Created Event Tag seeder with data from CERF

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call('UsersTableSeeder');
         $this->call('ActivitiesTableSeeder');
+        $this->call('EventTagsTableSeeder');
     }
 
 }
