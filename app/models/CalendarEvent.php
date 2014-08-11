2da9de62b7b5bcde4d1c4755f0bc99dc6e82981f
Joseph Le <josephle@me.com>
2014-08-11 14:35:49 -0700

    #14867396664257: Fixed and cleaned up Event model and renamed to CalendarEvent to avoid conflict with Laravel Event.

---
+++ app/models/CalendarEvent.php
@@ -0,0 +1,86 @@
+<?php
+
+use LaravelBook\Ardent\Ardent;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
+
+/**
+ * The CalendarEvent class contains a record of a single event and its properties
+ *
+ * @property integer $id
+ * @property integer $creator_id
+ * @property string $description
+ * @property string $event_location
+ * @property string $meeting_location
+ * @property \Carbon\Carbon $start_time
+ * @property \Carbon\Carbon $end_time
+ * @property \Carbon\Carbon $close_time
+ * @property string $deleted_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereCreatorId($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereEventLocation($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereMeetingLocation($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereStartTime($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereEndTime($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereCloseTime($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\CalendarEvent whereUpdatedAt($value)
+ */
+class CalendarEvent extends Ardent
+{
+
+    use SoftDeletingTrait;
+
+    protected $table = 'events';
+
+    protected $dates = array(
+        'start_time',
+        'end_time',
+        'close_time'
+    );
+
+    public $autoHydrateEntityFromInput = true;
+
+    public static $relationsData = array(
+//        'registrations'     => array(self::HAS_MANY, 'Registration'),
+        'creator' => array(
+            self::BELONGS_TO,
+            'User',
+            'foreign_key' => 'creator_id'
+        ),
+    );
+
+    public static $rules = array(
+        'creator_id' => 'required|exists:users,id'
+    );
+
+    /**
+     * Sets description of event
+     * @param $description
+     */
+    public function setDescriptionAttribute($description)
+    {
+        $this->attributes['description'] = strip_tags($description);
+    }
+
+    /**
+     * Sets location of event
+     * @param $location_event
+     */
+    public function setEventLocationAttribute($event_location)
+    {
+        $this->attributes['event_location'] = strip_tags($event_location);
+    }
+
+    /**
+     * Sets meeting location
+     * @param $location_meet
+     */
+    public function setMeetingLocationAttribute($meeting_location)
+    {
+        $this->attributes['meeting_location'] = strip_tags($meeting_location);
+    }
+}
