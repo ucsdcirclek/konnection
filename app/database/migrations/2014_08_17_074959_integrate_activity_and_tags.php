f71dc251a267f76dad08ecf11c39b08455672711
Joseph Le <josephle@me.com>
2014-08-17 01:24:21 -0700

    Added migration to integrate Activity and Event Tags with Event

---
+++ app/database/migrations/2014_08_17_074959_integrate_activity_and_tags.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IntegrateActivityAndTags extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('events_assigned_tags', function(Blueprint $table)
+		{
+            $table->integer('event_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('event_id')
+                ->references('id')->on('events')
+                ->onDelete('cascade');
+            $table->foreign('tag_id')
+                ->references('id')->on('event_tags');
+		});
+        Schema::table('activity_log', function(Blueprint $table)
+        {
+            $table->integer('event_id')->unsigned()->nullable();
+            $table->foreign('event_id')
+                ->references('id')->on('events')
+                ->onDelete('cascade');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('events_assigned_tags');
+        Schema::table('activity_log', function(Blueprint $table)
+        {
+            $table->dropColumn('event_id');
+        });
+	}
+
+}
