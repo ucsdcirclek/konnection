020b3013e08b9973606b252c61862becf9905bef
Joseph Le <josephle@me.com>
2014-09-20 22:31:44 -0700

    Fixed Event Transformer to output in ISO8601

--- app/transformers/EventTransformer.php
+++ app/transformers/EventTransformer.php
@@ -3,19 +3,20 @@
 class EventTransformer extends League\Fractal\TransformerAbstract
 {
 
-    public function transform(Event $event)
+    public function transform(CalendarEvent $event)
     {
         return array(
             'id' => (int)$event->id,
             'creator_id' => (int)$event->creator_id,
+            'title' => $event->title,
             'description' => $event->description,
             'event_location' => $event->event_location,
             'meeting_location' => $event->meeting_location,
-            'start_time' => $event->start_time,
-            'end_time' => $event->end_time,
-            'close_time' => $event->close_time,
-            'created_at' => $event->created_at,
-            'updated_at' => $event->updated_at
+            'start_time' => $event->start_time->toISO8601String(),
+            'end_time' => $event->end_time->toISO8601String(),
+            'close_time' => $event->close_time->toISO8601String(),
+            'created_at' => $event->created_at->toISO8601String(),
+            'updated_at' => $event->updated_at->toISO8601String()
         );
     }
 
