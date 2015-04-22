7a28778a1815286bf381fe85d2960cfad6a7496a
Joseph Le <josephle@me.com>
2015-04-21 18:47:35 -0700

    Fixed events seeder

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 use Faker\Factory as Faker;
 
@@ -16,12 +17,10 @@ class EventsTableSeeder extends Seeder
         DB::table('events')->delete();
 
         for ($i = 0; $i < 10; $i++) {
-            $start_time = $faker->dateTimeBetween('now', '10 years');
-            $end_time = new DateTime((string) date_format($start_time, 'Y-m-d H:i:s'));
-            $end_time->add(new DateInterval('PT10H'));
-            $close_time = new DateTime((string) date_format($start_time, 'Y-m-d H:i:s'));
-            $close_time->sub(new DateInterval('P10D'));
-            $open_time = new DateTime();
+            $start_time = Carbon::instance($faker->dateTimeBetween('now', '3 days'));
+            $end_time = Carbon::now()->addHours(3);
+            $close_time = Carbon::now()->subDay();
+            $open_time = Carbon::now();
 
             Event::create(
                 array(
@@ -30,10 +29,10 @@ class EventsTableSeeder extends Seeder
                     'description' => $faker->text,
                     'event_location' => $faker->address,
                     'meeting_location' => $faker->streetName,
-                    'start_time' => $start_time->format(DateTime::ISO8601),
-                    'end_time' => $end_time->format(DateTime::ISO8601),
-                    'close_time' => $close_time->format(DateTime::ISO8601),
-                    'open_time' => $open_time
+                    'start_time' => $start_time->toIso8601String(),
+                    'end_time' => $end_time->toIso8601String(),
+                    'close_time' => $close_time->toIso8601String(),
+                    'open_time' => $open_time->toIso8601String()
                 )
             );
         }
