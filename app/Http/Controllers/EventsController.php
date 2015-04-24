8c8a1345b2cc75ad858be997b451ee8626937d4c
Joseph Le <josephle@me.com>
2015-04-24 15:29:15 -0700

    Added EventsController

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,99 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use Carbon\Carbon;
+
+use App\Event;
+
+class EventsController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+        return view('pages.calendar');
+    }
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+        $event = Event::findOrFail($id);
+        $event->load('creator', 'registrations', 'guests');
+
+        // Look for events in the upcoming week
+        $range = [
+            Carbon::now(),
+            Carbon::now()->addWeek()
+        ];
+
+        $upcoming_events = Event::whereBetween('start_time', $range)->get();
+
+		return view('pages.event', compact('event', 'upcoming_events'));
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
