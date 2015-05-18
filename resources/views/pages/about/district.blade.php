428a79037e8ad81500f52b304215b5f07fbd913a
Joseph Le <josephle@me.com>
2015-05-18 15:08:52 -0700

    Added district, division, and membership pages

---
+++ resources/views/pages/about/district.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+
+@section('content)
+    <div class="wrapper">
+        <h1>About Our District</h1><br />
+        <p>
+            UCSD Circle K is a part of <strong>Cal-Nev-Ha</strong> district, which comprises California, Nevada, and Hawaii. CNH is the largest district in CKI. From the black sand beaches of Hawaii, the glamour of Hollywood, to the world's playground of Las Vegas, the California-Nevada-Hawaii District spans over 2700 miles to include some of the most dynamic and diverse parts of the world. With 40 clubs and over 1,000 members spanning these three states, Cal-Nev-Ha serves as a shining beacon to its fellow districts.
+
+        </p>
+        <p>
+            The first Circle K club was founded in 1947, and the California-Nevada-Hawaii District was one of the two founding Districts of Circle K International, along with the Michigan District. California-Nevada-Hawaii was instrumental in the internationalization of the organization. Chartered on December 12, 1957, the district was founded with thirty-one clubs and has grown to over forty clubs at the present.
+
+        </p>
+        <p>
+            Our district puts on many exciting events throughout the year, which you can find out more about on the Calendar
+
+        </p>
+        <p>
+            CNH Circle K's website can be found <a href="http://cnhcirclek.org/">here</a>.
+        </p>
+    </div>
+@endsection
---
+++ resources/views/pages/about/membership.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('content)
+    <div id="about" class="wrapper">
+        <h1>Become a Member</h1><br />
+        <p>We're really excited that you've decided to become a part of UCSD Circle K! To become a member,
+            you only need to follow these simple steps:</p>
+
+        <h2>Step 1: Complete the Membership Form</h2>
+        <p>We'll need some basic information about you as well as some interesting points about you to sort you into one
+            of the three families this year. Just download the form, fill it out and turn it in!</p>
+        <a class="button" target="_blank" href="/api/uploads/Membership%20Form%202014-2015.pdf">Download the Form</a>
+
+        <br />
+
+        <h2>Step 2: Submit your Dues</h2>
+        <p>Membership for the year costs only $40.
+            <strong>Turn this amount in check/cash along with your membership form.</strong>
+            Some of the benefits you'll receive are:</p>
+        <ul>
+            <li>A free club t-shirt</li>
+            <li>Free lunch on Fridays at Kiwanis Luncheon</li>
+            <li>Free rides to all events</li>
+            <li>You get to be sorted into a family!</li>
+        </ul>
+
+        <h2>Step 3: Submit the Above!</h2>
+        <p>That's it! You just need to submit the membership form along with your dues and you are an official,
+            dues paid member of UCSD Circle K! Your membership form and dues are due at the Week 3 GBM if you'd like to be
+            sorted into a family by the New Member Installation.</p>
+
+    </div>
+
+@endsection
