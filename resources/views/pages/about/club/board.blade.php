b4db8063892c37d501dd41b07b8c132c009a0c57
Joseph Le <josephle@me.com>
2015-05-18 15:30:15 -0700

    Fixed board images

--- resources/views/pages/about/club/board.blade.php
+++ resources/views/pages/about/club/board.blade.php
@@ -2,46 +2,46 @@
 
 @section('about-content')
     <div data-content="President &nbsp; Deborah Diaz" class="image">
-        <img src="images/board/Deborah.jpg"/>
+        <img src="{{ asset('images/board/Deborah.jpg') }}" />
     </div>
 
     <div data-content="VP Administration Jaime Estepa" class="image">
-        <img src="images/board/Jaime.jpg"/>
+        <img src="{{ asset('images/board/Jaime.jpg') }}" />
     </div>
 
     <div data-content="VP Service Taryn Conyers" class="image">
-        <img src="images/board/Taryn.jpg"/>
+        <img src="{{ asset('images/board/Taryn.jpg') }}" />
     </div>
 
     <div data-content="Secretary Emilie Shen" class="image">
-        <img src="images/board/Emilie.jpg"/>
+        <img src="{{ asset('images/board/Emilie.jpg') }}" />
     </div>
 
     <div data-content="Treasurer Victoria Ortiz " class="image">
-        <img src="images/board/Victoria.jpg"/>
+        <img src="{{ asset('images/board/Victoria.jpg') }}" />
     </div>
     <br>
 
     <div data-content="Fundraising Cyrielle Hacher" class="image">
-        <img src="images/board/Cyrielle.jpg"/>
+        <img src="{{ asset('images/board/Cyrielle.jpg') }}" />
     </div>
     <div data-content="Historian Tim Hwang" class="image">
-        <img src="images/board/Tim.jpg"/>
+        <img src="{{ asset('images/board/Tim.jpg') }}" />
     </div>
 
     <div data-content="Kiwanis Family Jon Woo" class="image">
-        <img src="images/board/Jon.jpg"/>
+        <img src="{{ asset('images/board/Jon.jpg') }}" />
     </div>
 
     <div data-content="Public Relations Nick Chang" class="image">
-        <img src="images/board/Nick.jpg"/>
+        <img src="{{ asset('images/board/Nick.jpg') }}" />
     </div>
     <div data-content="Social Andrew Thai" class="image">
-        <img src="images/board/Andrew.jpg"/>
+        <img src="{{ asset('images/board/Andrew.jpg') }}" />
     </div>
     <br>
 
     <div data-content="Technology Joseph Le" class="image">
-        <img src="images/board/Joseph.jpg"/>
+        <img src="{{ asset('images/board/Joseph.jpg') }}" />
     </div>
 @endsection
