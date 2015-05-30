e8b546ff0397f58c5b17fea1ad51a616087f5317
Joseph Le <josephle@me.com>
2015-05-29 18:32:00 -0700

    Added logout button

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,13 +11,16 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.1/fullcalendar.min.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.0/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.4/slick-theme.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/1.2.7/css/froala_editor.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.4/slick-theme.min.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/1.2.7/css/froala_editor.min.css">
     <link rel="stylesheet" href="{{ elixir("css/main.css") }}">
 </head>
 <body>
 <!--[if lt IE 8]>
-<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
 <![endif]-->
 
 <div id="main">
@@ -44,7 +47,8 @@
                             <li><a href="{{ url('about/district') }}">About</a></li>
                             <li><a target="_blank" href="http://dcon.cnhcirclek.org/">DCON</a></li>
                             <li><a target="_blank" href="http://ftc.cnhcirclek.org/">FTC</a></li>
-                            <li><a target="_blank" href="http://www.cnhcirclek.org/event/7-crazy_kompetition_for_infants_south/">CKI
+                            <li><a target="_blank"
+                                   href="http://www.cnhcirclek.org/event/7-crazy_kompetition_for_infants_south/">CKI
                                     South</a></li>
                         </ul>
                     </li>
@@ -56,10 +60,11 @@
                     </li>
                     <li class="nav-link"><a href="{{ url('contact') }}">contact</a></li>
                     @if (! Auth::check())
-                    <li class="nav-link"><a href="{{ url('/auth/login') }}">login</a></li>
-                    <li class="nav-link"><a href="{{ url('/auth/register') }}">register</a></li>
+                        <li class="nav-link"><a href="{{ url('/auth/login') }}">login</a></li>
+                        <li class="nav-link"><a href="{{ url('/auth/register') }}">register</a></li>
                     @else
-                    <li class="nav-link"><a href="{{ url('settings') }}">settings</a></li>
+                        <li class="nav-link"><a href="{{ url('settings') }}">settings</a></li>
+                        <li class="nav-link"><a href="{{ url('/auth/logout') }}">logout</a></li>
                     @endif
                 </ul>
                 <div id="corner-right"></div>
@@ -87,7 +92,8 @@
                 <a target="_blank" href="http://cnhcirclek.org"><img src="{{ asset('images/logos/cnh.png') }}"></a>
             </div>
             <div class="logo">
-                <a target="_blank" href="http://paradise.cnhcirclek.org/"><img src="{{ asset('images/logos/paradise.png') }}"></a>
+                <a target="_blank" href="http://paradise.cnhcirclek.org/"><img
+                            src="{{ asset('images/logos/paradise.png') }}"></a>
             </div>
         </div>
 
@@ -111,12 +117,12 @@
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 <script>
-//    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-//            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-//        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-//        e.src='https://www.google-analytics.com/analytics.js';
-//        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-//    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    //    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    //            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    //        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    //        e.src='https://www.google-analytics.com/analytics.js';
+    //        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    //    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
 </script>
 
 </body>
