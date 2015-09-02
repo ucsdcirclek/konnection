a8841312bcb2b0e1b42927ffaf193efb6cc2ce7d
Joseph Le <josephle@me.com>
2015-09-02 15:41:06 -0700

    Fixed mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'noreply@ucsdcki.org', 'name' => 'UCSD Circle K'],
 
 	/*
 	|--------------------------------------------------------------------------
