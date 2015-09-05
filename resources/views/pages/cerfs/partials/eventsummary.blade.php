e0a876cbc68a3be28d4ea1965261fc085fd16539
Brian Tan <tanrui.brian@gmail.com>
2015-09-05 16:50:55 +0000

    Split form into partials

---
+++ resources/views/pages/cerfs/partials/eventsummary.blade.php
@@ -0,0 +1,34 @@
+<div id="event-summary">
+
+    {!! Form::hidden('event_id', $event->id) !!}
+    {!! Form::hidden('chair_id', null, array('class' => 'chair-field')) !!}
+
+    <div class="wrapper">
+
+        <div class="description">
+            <h1>{{ $event->title }}</h1>
+
+            <p class="half"><strong>Date:</strong> {{ $event->start_time->setTimezone('America/Los_Angeles')->format('F n, Y') }}</p>
+            <p class="half"><strong>Location:</strong> {{ $event->event_location }}</p>
+        </div>
+
+        <div class="chair avatar large">
+
+            @if (is_null($chair))
+                <p><strong>No chair</strong></p>
+
+                <img src="/avatars/original/missing.png">
+            @else
+                <p><strong>Chair:</strong></p>
+                <p>{{ $chair->first_name }}</p>
+
+                <img src="{{ $chair->avatar->url() }}">
+            @endif
+
+            <div><a href=".search-popup" class="user-not-optional search-popup-link button emphasis" data-effect="mfp-move-horizontal">Change</a></div>
+
+            @include('search.search')
+
+        </div>
+    </div>
+</div>
---
+++ resources/views/pages/cerfs/partials/eventtags.blade.php
@@ -0,0 +1,91 @@
+<div id="event-tags-section">
+    <div class="wrapper">
+        <h3>Event Tags</h3>
+
+        <p class="section-instructions">
+            All Circle K events fall into one of three categories: service, administration, and/or social. These
+            three categories correspond to our three tenets: service, leadership, and fellowship. Depending on the
+            nature of the event, members who attend the event can get service hours, administrative hours, social
+            hours, or a combination of the three.
+        </p>
+
+        <div class="tags-table">
+            <div class="tags-table-header">
+                <h4>Service</h4>
+
+                <small>
+                    Any service event that receives service hours should be tagged as CO, but depending on the
+                    nature of the event, the event can be tagged with the other service tags.
+                </small>
+            </div>
+
+            @foreach ($service_tags as $tag)
+                <div class="tags-content-row">
+                    <div>{!! Form::checkbox('service_tags[]', $tag->abbreviation) !!}</div>
+                    <div><p>{{ $tag->name }} ({{ $tag->abbreviation }})</p></div>
+                    <div><p>{{ $tag->description }}</p></div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="tags-table">
+            <div class="tags-table-header">
+                <h4>Leadership</h4>
+
+                <small>
+                    Any event related to the operation of the club should be tagged as AD. Examples of
+                    administrative events include – but are not limited to - attending meetings (e.g.) board
+                    meetings, committee meetings, Kiwanis meetings, tabling, and workshops.
+                </small>
+            </div>
+
+            @foreach ($admin_tags as $tag)
+                <div class="tags-content-row">
+                    <div>{!! Form::checkbox('admin_tags[]', $tag->abbreviation) !!}</div>
+                    <div><p>{{ $tag->name }} ({{ $tag->abbreviation }})</p></div>
+                    <div><p>{{ $tag->description }}</p></div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="tags-table">
+            <div class="tags-table-header">
+                <h4>Fellowship</h4>
+
+                <small>
+                    Any event in which club members are socially interacting with each other should be tagged as
+                    SE. A social event promotes the morale of its members so it is usually tagged as MD; however,
+                    remember that although all SE events are MD tagged, not all MD events are SE tagged (e.g.
+                    workshops).
+                </small>
+            </div>
+
+            @foreach ($social_tags as $tag)
+                <div class="tags-content-row">
+                    <div>{!! Form::checkbox('social_tags[]', $tag->abbreviation) !!}</div>
+                    <div><p>{{ $tag->name }} ({{ $tag->abbreviation }})</p></div>
+                    <div><p>{{ $tag->description }}</p></div>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="tags-table">
+            <div class="tags-table-header">
+                <h4>Miscellaneous</h4>
+
+                <small>
+                    Miscellaneous tags are supplementary. Remember that all events have to fall in one of the above
+                    three categories before getting a supplementary tag.
+                </small>
+            </div>
+
+            @foreach ($misc_tags as $tag)
+                <div class="tags-content-row">
+                    <div>{!! Form::checkbox('misc_tags[]', $tag->abbreviation) !!}</div>
+                    <div><p>{{ $tag->name }} ({{ $tag->abbreviation }})</p></div>
+                    <div><p>{{ $tag->description }}</p></div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
