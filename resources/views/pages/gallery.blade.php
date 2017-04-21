65ccdfb83d2d591135db23c0f3a66fa4e6f6953c
wex038 <wex038@ucsd.edu>
2017-04-21 11:20:06 -0700

    Added in gallery.blade.php
    This creates the front end page view for the gallery page

---
+++ resources/views/pages/gallery.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+    Gallery
+@endsection
+@section('content')
+    @include('layouts.header', array('headerTitle' => 'Welcome to the UCSD CKI Gallery!'))
+
+@endsection
