3cff0bad2b900235d6f1040c91ffa7e4acad5f3e
Weijin Xu <wex038@ucsd.edu>
2017-09-25 18:57:35 -0700

    Updated information and format

--- resources/views/pages/about/club/general.blade.php
+++ resources/views/pages/about/club/general.blade.php
@@ -11,23 +11,37 @@
     <p>First chartered in 1977, Circle K International at UC San Diego is the largest community service organization
         on campus, boasting over 150 members during the 2013-2014 school year. UCSD CKI received the Outstanding
         Community Service Program of the Year Award from UCSD for 2010-2011. UCSD CKI distinguishes itself from other
-        campus organizations for its dedication to service, leadership, and fellowship. Its doors are always open to new
-        members, so you can get involved today! Our club mascot is Crobie, a microbe that is also known as a stomach
-        virus and. Our club theme for the 2016-2017 year is Adventure! 2015-2016 year was Under the Sea, 2014-2015 year was Fairy Tales,
-        2012-2013 year was Spies and for the 2014-2015, it was The Nightmare Before Christmas. </p>
+        campus organizations for its dedication to <strong>service</strong>, <strong>leadership</strong>, and
+        <strong>fellowship</strong>. Its doors are always open to new members, so you can get involved today!
+    </p>
+    
+    <p>
+        Our club mascot is Crobie, a microbe that is also known as a stomach virus and our club theme for the 2017-2018 
+        year is Blast to the Past!
+    </p>
+    
+   <h4>Past Club Themes</h4>
+        <ul>
+            <li>2016-2017: Adventure</li>
+            <li>2015-2016: Under the Sea</li>
+            <li>2014-2015: Fairy Tales</li>
+            <li>2012-2013: Spies</li>
+            <p>2014-2015: The Nightmare Before Christmas</p>
+        </ul>
 
     <h4>General Body Meetings</h4>
 
     <p>UCSD Circle K hold general body meetings every Monday at 8PM (unless there is a holiday) to discuss upcoming
-        events, recognize members, and all around just have fun. Meetings are open to everyone.</p>
+        events, recognize members, and all around just have fun. Meetings are open to everyone. Meeting location
+        will be announced either on our facebook page or on the event calender.</p>
 
     <h4>Key Events</h4>
 
-    <p>UCSD Circle K puts on hosts a Masquerade Ball fundraiser annually during Fall Quarter. Two years ago,
-        it took place on November 15th on a triple decker yacht. Last year, it raised over $11,000
-        in proceeds for charity.
-        During fall quarter, the club hosts its New Member Installation, a dinner reception that welcomes new members into our club.
-        {{--During Winter Quarter, the club hosts its annual Snowcial, a weekend ski trip to Big Bear.--}}
-        UCSD Circle K also participates in many District Events, which you can find out more about on the calendar
-        page.</p>
+    <p>UCSD Circle K puts on hosts a <strong>Masquerade Ball</strong> fundraiser annually during Fall Quarter. Two years ago,
+        it took place on November 15th on a triple decker yacht. Last year, it raised over $11,000 in proceeds for charity.
+        During fall quarter, the club hosts its <strong>New Member Installation</strong>, a dinner reception that welcomes 
+        new members into our club.
+        UCSD Circle K also participates in many District Events such as <strong>District Convention</strong> (DCON), 
+        <strong>Fall Training Conference</strong> (FTC), <strong>CKI South</strong>, <strong>District Large Scale Service Project</strong>
+        (DLSSP), and <strong>Crazy Kompetition for Infants</strong> which you can find out more about on the District tab.</p>
 @endsection
