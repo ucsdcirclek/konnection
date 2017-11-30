444a986b059fdcdca8b8afffc910c5af43f89afb
Weijin Xu <wex038@ucsd.edu>
2017-11-29 16:36:34 -0800

    Added Member of the Month Section

--- resources/views/pages/halloffame/halloffame.blade.php
+++ resources/views/pages/halloffame/halloffame.blade.php
@@ -6,24 +6,24 @@
 
 @section('content')
     <div class="member-row2">
-    <!--
 
         <a href="{{ url('mom') }}">
-            <div>
-                <p><strong>Member of the Month</strong></p>
+            <div class="container">
+            <img src="https://fontmeme.com/permalink/171126/1f9f638f093c130cda56b366bf8864ae.png" alt="fancy-fonts"
+                 border="0" class="image">
             </div>
         </a>
--->
+        <hr style="height: 3px">
         <a href="{{url('spotlight')}}">
             <div class="container">
-                <img src="https://fontmeme.com/permalink/171025/2b4fb9b5cca3b7d6595e48f5999e8efd.png" alt="fancy-fonts"
+                <img src="https://fontmeme.com/permalink/171126/bb699f346bec919bc37d5b2e467ca185.png" alt="fancy-fonts"
                      border="0" class="image">
             </div>
         </a>
         <hr style="height: 3px">
         <a href="{{ url('sof') }}">
             <div class="container">
-                <img src="https://fontmeme.com/permalink/171025/40e1872e881133d406175c766ddef39c.png" alt="fancy-fonts"
+                <img src="https://fontmeme.com/permalink/171126/b1ab8963591c23686f3af1912c58895d.png" alt="fancy-fonts"
                      border="0" class="image">
             </div>
         </a>
