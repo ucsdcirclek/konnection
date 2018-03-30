bb23d15b0e8ae44fe933057a2ee383cd280e1aa8
Weijin Xu <wex038@ucsd.edu>
2018-03-29 18:52:14 -0700

    Updated Spotlight Page

--- resources/views/pages/halloffame/spotlight.blade.php
+++ resources/views/pages/halloffame/spotlight.blade.php
@@ -33,10 +33,50 @@
 
     <div class="wrapper">
         <div class="title-wrapper">
-            @include('layouts.header', array('headerTitle' => 'October'))
+            @include('layouts.header', array('headerTitle' => 'April 2017'))
         </div>
         <div class="mobile-text">
             <div class ="contact-row">
+                <div>
+                    <img src="{{ asset('images/Committees/SLSSP/Victoria.jpg') }}" />
+                    <p><strong>Victoria Vu</strong></p>
+                    <p>1st Year</p>
+                    <p>Cognitive Science</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="title-wrapper">
+            @include('layouts.header', array('headerTitle' => 'May 2017'))
+        </div>
+        <div class="mobile-text">
+            <div class="contact-row">
+                <div>
+                    <img src="{{ asset('images/Committees/SLSSP/Alyssa.jpg') }}" />
+                    <p><strong>Alyssa Lew</strong></p>
+                    <p>1st Year</p>
+                    <p>General Biology</p>
+                </div>
+                <div>
+                    <img src="{{ asset('images/halloffame/mr/spotlight/Miguel.jpg')}}" />
+                    <p><strong>Miguel Gamo</strong></p>
+                    <p>3rd Year</p>
+                    <p>Environmental Chemistry</p>
+                </div>
+                <div>
+                    <img src="{{ asset('images/halloffame/mr/spotlight/Tammy.jpg')}}" />
+                    <p><strong>Tammy De</strong></p>
+                    <p>2nd Year</p>
+                    <p>Mechanical Engineering</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="title-wrapper">
+            @include('layouts.header', array('headerTitle' => 'October 2017'))
+        </div>
+        <div class="mobile-text">
+            <div class="contact-row">
                 <div>
                     <img src="{{ asset('images/halloffame/mr/spotlight/Alison.jpg') }}" />
                     <p><strong>Alison Liu</strong></p>
@@ -53,10 +93,10 @@
         </div>
 
         <div class="title-wrapper">
-            @include('layouts.header', array('headerTitle' => 'November'))
+            @include('layouts.header', array('headerTitle' => 'November 2017'))
         </div>
         <div class="mobile-text">
-            <div class ="contact-row">
+            <div class="contact-row">
                 <div>
                     <img src="{{ asset('images/halloffame/mr/spotlight/Wes.jpg') }}" />
                     <p><strong>Wes Yuen</strong></p>
@@ -73,10 +113,10 @@
         </div>
 
         <div class="title-wrapper">
-            @include('layouts.header', array('headerTitle' => 'January'))
+            @include('layouts.header', array('headerTitle' => 'January 2018'))
         </div>
         <div class="mobile-text">
-            <div class ="contact-row">
+            <div class="contact-row">
                 <div>
                     <img src="{{ asset('images/halloffame/mr/spotlight/Maricris.jpg') }}" />
                     <p><strong>Maricris Hernandez</strong></p>
@@ -88,6 +128,40 @@
                     <p><strong>Nayeli Mota</strong></p>
                     <p>2nd year</p>
                     <p>Language Studies</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="title-wrapper">
+            @include('layouts.header', array('headerTitle' => 'February 2018'))
+        </div>
+        <div class="mobile-text">
+            <div class="contact-row">
+                <div>
+                    <img src="{{asset('images/Committees/SLSSP/Jovonne.jpg')}}" />
+                    <p><strong>Jovonne Lee</strong></p>
+                    <p>2nd Year</p>
+                    <p>Human Biology</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="title-wrapper">
+            @include('layouts.header', array('headerTitle' => 'March 2018'))
+        </div>
+        <div class="mobile-text">
+            <div class="contact-row">
+                <div>
+                    <img src="{{asset('images/impactteams/teampulse/Sally.jpg')}}" />
+                    <p><strong>Sally Rong</strong></p>
+                    <p>1st Year</p>
+                    <p>Political Science</p>
+                </div>
+                <div>
+                    <img src="{{asset('images/halloffame/mr/spotlight/Vanissa.jpg')}}" />
+                    <p><strong>Vanissa Tsang</strong></p>
+                    <p>2nd Year</p>
+                    <p>ESYS - Ecology, Behavior, and Evolution</p>
                 </div>
             </div>
         </div>
