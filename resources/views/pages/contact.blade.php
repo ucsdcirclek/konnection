7b3d56b13523abd84854b850dd5899a2d826ac32
Weijin Xu <wex038@ucsd.edu>
2018-04-01 00:49:16 -0700

    Changed contact to passion contact

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -25,103 +25,100 @@
         </div>
         <div class="contact-row">
             <div>
-                <img src="{{ asset('images/board/bradley.jpg') }}" />
-                <p><strong>Bradley Ventayen</strong></p>
+                <img src="{{ asset('images/passionfruits/Bradley.jpg') }}" />
+                <p><strong>Supereme Leader Overlord</strong></p>
                 <p>President</p>
                 <p>president@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/john.jpg') }}" />
-                <p><strong>John Shin</strong></p>
+                <img src="{{ asset('images/passionfruits/John.jpg') }}" />
+                <p><strong>Grandpa John</strong></p>
                 <p>Vice President of Administration</p>
                 <p>vpa@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/matthew.jpg') }}" />
-                <p><strong>Matthew Kawakami</strong></p>
+                <img src="{{ asset('images/passionfruits/Matthew.jpg') }}" />
+                <p><strong>Thirsty for Service</strong></p>
                 <p>Vice President of Service</p>
                 <p>vps@ucsdcki.org</p>
             </div>
         </div>
         <div class="contact-row">
             <div>
-                <img src="{{ asset('images/board/wesley.jpg') }}" />
-                <p><strong>Wesley Wu</strong></p>
+                <img src="{{ asset('images/passionfruits/Wesley.jpg') }}" />
+                <p><strong>Wesles</strong></p>
                 <p>Secretary</p>
-                {{--<p>3rd Year</p>
-                <p>Communications</p>
-                --}}
                 <p>secretary@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/nancy.jpg') }}" />
-                <p><strong>Nancy Huang</strong></p>
+                <img src="{{ asset('images/passionfruits/Nancy.jpg') }}" />
+                <p><strong>Money is my Passion</strong></p>
                 <p>Treasurer</p>
                 <p>treasurer@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/anh.jpg') }}" />
-                <p><strong>Anh Vo</strong></p>
+                <img src="{{ asset('images/passionfruits/Anh.jpg') }}" />
+                <p><strong>Anhvocado</strong></p>
                 <p>Fundraising</p>
                 <p>fundraising@ucsdcki.org</p>
             </div>
         </div>
         <div class="contact-row">
             <div>
-                <img src="{{ asset('images/board/poiema.jpg') }}" />
-                <p><strong>Poiema Kim</strong></p>
+                <img src="{{ asset('images/passionfruits/Poi.jpg') }}" />
+                <p><strong>Mushed Taro</strong></p>
                 <p>Historian</p>
                 <p>historian@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/michael.jpg') }}" />
-                <p><strong>Michael Christensen</strong></p>
+                <img src="{{ asset('images/passionfruits/Michael.jpg') }}" />
+                <p><strong>Michael Anthoney Gonzales Christensen</strong></p>
                 <p>Kiwanis Family</p>
                 <p>kfam@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/shannon.jpg') }}" />
-                <p><strong>Shannon Lee</strong></p>
+                <img src="{{ asset('images/passionfruits/Shannon.jpg') }}" />
+                <p><strong>Ms.PResident</strong></p>
                 <p>Public Relations</p>
                 <p>pr@ucsdcki.org</p>
             </div>
         </div>
         <div class="contact-row">
             <div>
-                <img src="{{ asset('images/board/erica.jpg') }}" />
-                <p><strong>Erica Wei</strong></p>
+                <img src="{{ asset('images/passionfruits/Erica.jpg') }}" />
+                <p><strong>De Wei to MR</strong></p>
                 <p>Member Relations</p>
                 <p>mr@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/louise1.jpg') }}" />
-                <p><strong>Louise Tolentino</strong></p>
+                <img src="{{ asset('images/passionfruits/Louise.jpg') }}" />
+                <p><strong>Tol-en-tiny</strong></p>
                 <p>Member Development and Education</p>
                 <p>mde@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/don.jpg') }}" />
-                <p><strong>Don Tran</strong></p>
+                <img src="{{ asset('images/passionfruits/Don.jpg') }}" />
+                <p><strong>FB Game Master</strong></p>
                 <p>Social</p>
                 <p>social@ucsdcki.org</p>
             </div>
         </div>
         <div class="contact-row">
             <div>
-                <img src="{{ asset('images/board/ryan.jpg') }}" />
-                <p><strong>Ryan Luong</strong></p>
+                <img src="{{ asset('images/passionfruits/Ryan.jpg') }}" />
+                <p><strong>Do it You Won't</strong></p>
                 <p>Fellowship</p>
                 <p>fellowship@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/siobhan.jpg') }}" />
-                <p><strong>Siobhán Lin-Nugent</strong></p>
+                <img src="{{ asset('images/passionfruits/Siobhan.jpg') }}" />
+                <p><strong>Savage Meme Master</strong></p>
                 <p>Service</p>
                 <p>service@ucsdcki.org</p>
             </div>
             <div>
-                <img src="{{ asset('images/board/weijin.jpg') }}" />
-                <p><strong>Weijin Xu</strong></p>
+                <img src="{{ asset('images/passionfruits/Weijin.jpg') }}" />
+                <p><strong>Vertical Angle</strong></p>
                 <p>Technology</p>
                 <p>technology@ucsdcki.org</p>
                 <p></p>
