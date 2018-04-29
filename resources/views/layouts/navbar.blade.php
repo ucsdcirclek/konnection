d9906767a8bf02c41f1e931fbb771072ae72e914
carld <cdungca@ucsd.edu>
2018-04-28 17:12:31 -0700

    Added modal box - Added modal.js script - Added _modal-login.scss - Added modal login box to _navigation.scss - Included _modal-login.scss to main.scss - Added modal login code to navbar.blade.php - Included modal.js in gulpfile.js

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,15 @@
 <style>
     @media only screen and (max-width: 500px) {
         .mobile-text {
-            font-size: 8px;
+            font-size: 12px;
+        }
+        .centered-navigation {
+            height: 18%;
+        }
+        .modal-content {
+            height: 480px;
+            width: 80%;
+            overflow: auto;
         }
     }
 </style>
@@ -54,8 +62,8 @@
 
 
                 @if (! Auth::check())
-                    <li class="nav-link"><a href="{{ url('/auth/login') }}">login</a></li>
-                    <li class="nav-link"><a href="{{ url('/auth/register') }}">register</a></li>
+                    <li class="nav-link" id="ModalLogin"><a>login</a></li>
+                   <!-- <li class="nav-link"><a href="{{ url('/auth/register') }}">register</a></li> -->
                 @else
                     <li class="nav-link"><a href="{{ url('settings') }}">account</a></li>
                     <li class="nav-link"><a href="{{ url('/auth/logout') }}">logout</a></li>
@@ -67,4 +75,106 @@
 
       </div>
     </div>
+
+
+    <!-- Login button at the top of the page triggers the modal -->
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div>
+                <button onclick="openTab('loginForm')">Login</button>
+                <button onclick="openTab('registerForm')">Register</button>
+            </div>
+            <div id="login" class="wrapper">
+
+
+                <!--Login form within the modal content -->
+                <div id="loginForm" class="loginTab"> <!--Removed ID to have login box only -->
+                    <form role="form" method="POST" action="{{ url('/auth/login') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <h3>Account Access</h3>
+                        <hr />
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <label for="email">Email</label>
+                        <input type="email" name="email" value="{{ old('email') }}">
+                        <label for="password">Password</label>
+                        <input type="password" name="password">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember"> Remember Me
+                                <p></p>
+                                <input type="checkbox" name="remember"> Take me to calendar
+                            </label>
+                        </div>
+                        <br />
+                        <button type="submit" class="button">Login</button>
+                        <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+                    </form>
+                </div>
+
+                <div id="registerForm" class="loginTab" style="display:none"> <!--Removed ID so that this div doesn't display as a column -->
+                    <div class="wrapper">
+                        <h2>Let's get registered!</h2>
+
+                        <p>You'll need to register for an account before being able to sign up for an event! We'll keep your information safe and only use it to give you updates on the events you signed up for!</p>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form role="form" method="POST" action="{{ url('/auth/register') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                            <label for="username">Username</label>
+                            <input type="text" name="username" value="{{ old('username') }}">
+
+                            <label for="email">Email</label>
+                            <input type="email" name="email" value="{{ old('email') }}">
+
+                            <label for="password">Password</label>
+                            <input type="password" name="password">
+
+                            <label for="password_confirm">Password Confirmation</label>
+                            <input type="password" name="password_confirmation">
+
+                            <label for="first_name">Phone Number</label>
+                            <input type="text" name="phone" value="{{ old('phone') }}">
+
+                            <label for="first_name">First Name</label>
+                            <input type="text" name="first_name" value="{{ old('first_name') }}">
+
+                            <label for="last_name">Last Name</label>
+                            <input type="text" name="last_name" value="{{ old('last_name') }}">
+
+                            {!! Recaptcha::render() !!}
+                            <br />
+
+                            <input type="submit" class="button" value="Register">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </header>
