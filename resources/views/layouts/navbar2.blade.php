7ce478b1ec32ea0bf6c0c75d8cc01dc5262140ec
carld <cdungca@ucsd.edu>
2018-06-25 03:42:24 +0000

    removed brand name from desktop view

--- resources/views/layouts/navbar2.blade.php
+++ resources/views/layouts/navbar2.blade.php
@@ -6,45 +6,54 @@
         <nav>
             <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
             <ul class="nav-list">
-                <li>
-                    <a href="#!">Home</a>
+                <li class="nav-link">
+                    <a href="#!">home</a>
                 </li>
-                <li>
-                    <a href="#!">About</a>
+                <li class="nav-link">
+                    <a href="#!">about</a>
                 </li>
-                <li>
-                    <a href="#!">Services</a>
+                <li class="nav-link">
+                    <a href="#!">calendar</a>
                     <ul class="nav-dropdown">
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Web Design</a>
                         </li>
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Web Development</a>
                         </li>
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Graphic Design</a>
                         </li>
                     </ul>
                 </li>
-                <li>
-                    <a href="#!">Pricing</a>
+                <li class="nav-link">
+                    <a href="#!">resources</a>
                 </li>
-                <li>
-                    <a href="#!">Portfolio</a>
+                <li class="nav-link">
+                    <a href="#!">bulletin</a>
                     <ul class="nav-dropdown">
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Web Design</a>
                         </li>
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Web Development</a>
                         </li>
-                        <li>
+                        <li class="nav-link">
                             <a href="#!">Graphic Design</a>
                         </li>
                     </ul>
                 </li>
-                <li>
-                    <a href="#!">Contact</a>
+                <li class="nav-link">
+                    <a href="#!">district</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#!">contact</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#!">gallery</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#!">login</a>
                 </li>
             </ul>
         </nav>
