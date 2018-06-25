ae20ea2cb06e63f263ae49d88e6300e4ff38a652
carld <cdungca@ucsd.edu>
2018-06-25 21:51:27 +0000

    Made admin bar mobile friendly

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,3 +3,12 @@
         <a href="{{ url('admin') }}">Admin</a>
     </div>
 @endif
+
+<style>
+    @media only screen and (max-width: 500px) {
+        #admin-link { /*Attaches admin button to mobile nav bar */
+            top: 4.2em;
+            z-index: 9999;
+        }
+    }
+</style>
