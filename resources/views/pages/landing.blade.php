1b6025176f24524c630489cd5c98f9730db4318a
carld <cdungca@ucsd.edu>
2018-07-29 07:46:31 +0000

    Centered page content

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,32 +1,98 @@
 <!DOCTYPE html>
   <html class="no-js" lang="">
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Landing | UCSD Circle K</title>
         <meta name="description" content="@yield('description')">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <!-- Bootstrap -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <!-- End bootstrap -->
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
     @section('description')
         Established in 1977, Circle K International at UCSD is a community service organization offering service, social and leadership opportunities.
     @endsection
 
-
     <body>
 
+      <div class="jumbotron">
+          <h1 class="text-center">UCSD Circle K International</h1>
+          <h1 class="text-center">2018-2019</h1>
+          <h2 class="text-center">To Service and Beyond!</h2>
+          <div class="container" align="center">
+            <button type="button" class="btn btn-primary btn-lg">First Time Here?</button>
+          </div>
+          <div class="container" align="center">
+              <div class="row">
+                  <button type="button" class="btn btn-primary btn-lg">Home</button>
+                  <button type="button" class="btn btn-primary btn-lg">Calendar</button>
+                  <button type="button" class="btn btn-primary btn-lg">Resources</button>
+                  <button type="button" class="btn btn-primary btn-lg">Login</button>
+              </div>
+          </div>
+      </div>
+
+      <div class="container-fluid">
+          <h1 class="text-center">Who are we?</h1>
+          <div class="row">
+              <div class="col-sm-4" align="center">
+                  <i class="fas fa-home"></i>
+                  <h2>1</h2>
+                  <h2>Organization</h2>
+              </div>
+              <div class="col-sm-4" align="center">
+                  <i class="fas fa-globe"></i>
+                  <h2>13,000+</h2>
+                  <h2>Worldwide Members</h2>
+              </div>
+              <div class="col-sm-4" align="center">
+                  <i class="fas fa-hands-helping"></i>
+                  <h2>199,327+</h2>
+                  <h2>Total Service Hours</h2>
+              </div>
+          </div>
+          <div class="container" align="center">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.</p>
+          </div>
+      </div>
+
+      <div class="container">
+        <img src="{{ asset('images/Committees/SAAT/SAATCover17182.jpg') }}" />
+      </div>
+
+    <h1 class="text-center">What does UCSD CKI do?</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4" align="center">
+                <button type="button" class="btn btn-primary btn-lg">Service</button>
+            </div>
+            <div class="col-sm-4" align="center">
+                <button type="button" class="btn btn-primary btn-lg">Leadership</button>
+            </div>
+            <div class="col-sm-4" align="center">
+                <button type="button" class="btn btn-primary btn-lg">Fellowship</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid" align="center">
+        <h1>Join our community</h1>
+        <button type="button" class="btn btn-primary btn-lg">Get involved</button>
+    </div>
+
+
     </body>
+
 
   </html>
