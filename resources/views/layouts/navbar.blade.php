f48da9a869b521087c76632d540b28d8bcb5fc08
vagrant <vagrant@homestead>
2018-08-30 22:28:44 +0000

    Revised District Page and completed NavBar Organization

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,19 +9,23 @@
 
         }
         .modal-content { /* Dimensions of the login modal box */
-            height: 480px;
+            <!--480--->
+            height: 1480px;
             width: 90%;
             overflow: auto;
         }
 
         li.modalTab a {
-            padding: 24px 16px;
+            padding: 24px 60px;
         }
 
         .nav-list {
-            height: 75vh; /* Allows the mobile navbar to be scrolled if too big */
+            <!--75--->
+            height: 175vh; /* Allows the mobile navbar to be scrolled if too big */
             overflow-y: scroll;
         }
+
+
     }
 
 </style>
@@ -45,7 +49,7 @@
                         <li><a href="{{ url('about/circlek') }}">Circle K</a></li>
                         <li><a href="{{ url('about/club') }}">Club</a></li>
                         <li><a href="{{ url('gallery') }}">Gallery</a></li>
-                        <!------- DISTRICT LINK HERE -------------->
+                        <li><a href="{{ url('about/district') }}">District</a></li>
                         <li><a href="{{ url('about/membership') }}">Membership</a></li>
                         <li><a target="_blank" href="http://www.kiwanis.org">Kiwanis</a></li>
                         <li><a target="_blank" href="http://www.kiwanisclublajolla.org/">La Jolla Kiwanis</a></li>
@@ -67,20 +71,6 @@
                 </li>
                 <li>
                     <a href="{{ url('contact') }}">contact</a>
-                </li>
-
-
-
-                <li>
-                    <a>district</a>
-                    <ul class="nav-dropdown">
-                        <li><a href="{{ url('about/district') }}">About</a></li>
-                        <li><a target="_blank" href="http://dcon.cnhcirclek.org/">DCON</a></li>
-                        <li><a target="_blank" href="http://ftc.cnhcirclek.org/">FTC</a></li>
-                        <li><a target="_blank"
-                               href="http://www.cnhcirclek.org/committees/fifun/crazy-kompetition-2017-games-playbook/">CKI
-                                South</a></li>
-                    </ul>
                 </li>
 
 
