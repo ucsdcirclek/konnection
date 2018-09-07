a84401728fb1f3a9d90975e290b77469ac30a41b
Carl Dungca <cdungca@ucsd.edu>
2018-09-07 01:25:08 -0700

    Revert "Develop" (#132)
    * Revert "Revert "Resolve conflicts (#130)" (#131)"
    
    This reverts commit 4cb936f04d05c6ed225ed125523f1792b12a84a1.
    
    * Revert "Resolve conflicts (#130)"
    
    This reverts commit a472513c2caa48d3028c4a7a31f162db218b74c7.
    
    * Revert "Website re-theme (#129)"
    
    This reverts commit e388b1eea75422b3af46b812b86c528c20c49033.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -139,8 +139,6 @@ Route::get('TeamFTK', function() { return view( 'pages.impactteams.teamftk'); })
 Route::get('TeamPulse', function() { return view( 'pages.impactteams.teampulse'); });
 Route::get('Team_Paws', function() { return view( 'pages.impactteams.team_paws'); });
 
-//Route::get('landing', function() { return view( 'pages.landing'); });
-
 
 
 /*
