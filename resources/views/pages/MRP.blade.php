b78d0ddffb01e632f6f1991a16f4abab70f6ceb0
carld <cdungca@ucsd.edu>
2018-09-10 22:36:53 +0000

    Added MRP page route and link in navbar

---
+++ resources/views/pages/MRP.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+    MRP
+@endsection
+
+@section('content')
+    @include('layouts.header', array('headerTitle' => 'Membership Recognition Program'))
+    <p>MRP</p>
+@endsection
