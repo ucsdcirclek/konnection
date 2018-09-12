c5b1ef863d296e01f2a7dc753db224b7a406561c
carld <cdungca@ucsd.edu>
2018-09-12 23:04:53 +0000

    Re-centered who's going header

--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -197,7 +197,7 @@
                 </div>
 
             <div class="registrations">
-                <h4>Who's going</h4>
+                <h4 style="padding-right: 5%;">Who's going</h4>
                 <ul>
                     @foreach($event->registrations as $registration)
                         <li class="avatar small">
