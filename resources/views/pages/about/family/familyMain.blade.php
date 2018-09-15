d77598ea59edb7e6d0cc13855fb79ce711d9e08b
vagrant <vagrant@homestead>
2018-09-15 20:56:05 +0000

    added family page layout

---
+++ resources/views/pages/about/family/familyMain.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.master')
+
+@section('title')
+    Contact
+@endsection
+
+@section('content')
+    @include('layouts.header', array('headerTitle' => 'Family System'))
+
+    <style>
+     p.descr{
+         white-space: pre-line;
+     }
+
+     p.big{
+         line-height: 200%;
+     }
+
+    </style>
+
+    <div class="wrapper">
+        <div class="title-wrapper">
+            <h1 class="title">A Home Away From Home</h1>
+	</div>
+
+
+
+
+   <div class="member-row" style="width: 100% %">
+
+
+
+                                <div id="slideshow">
+                                    <div>
+                                        <img src="https://i.imgur.com/iH9URcZ.jpg" />
+                                    </div>
+                                    <div>
+                                        <img src="https://i.imgur.com/bqJ06og.jpg" />
+                                    </div>
+                                    <div>
+                                        <img src=" https://i.imgur.com/12kbu7K.jpg" />
+                                    </div>
+                                    <div>
+                                        <img src=" https://i.imgur.com/WMKkTnr.jpg  " />
+                                    </div>
+				</div>
+
+</div>
+	
+<br>
+<br>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis semper nisi, vel posuere tellus. Nunc consectetur libero eu erat consequat, eget congue tortor hendrerit. Maecenas vitae ullamcorper ex. Mauris faucibus neque ut tellus faucibus, in dictum sem efficitur. Etiam consectetur vulputate arcu mattis placerat. Integer placerat mi sit amet iaculis euismod. Aliquam dapibus lacus sit amet turpis lacinia, nec rutrum nisi rhoncus. Pellentesque luctus quis risus a euismod. Vivamus a quam tempor, consectetur purus id, gravida tortor. Vivamus a justo eu ligula tristique pretium ut ut ex. Proin ac gravida ligula, eget auctor mi. Integer vel massa tincidunt, congue lectus sed, semper ex. Fusce tincidunt risus vel faucibus mattis. Aliquam ut arcu lacinia, placerat ex pellentesque, vehicula risus. Morbi congue nec tellus ut elementum. Suspendisse interdum odio non feugiat luctus.
+
+Vestibulum lobortis rutrum sem, eu pellentesque purus facilisis sit amet. Pellentesque dui sapien, scelerisque ut erat sed, suscipit consequat tellus. Proin justo mi, varius quis rhoncus et, aliquam a ipsum. Vestibulum et ipsum placerat, consectetur orci eu, sollicitudin ex. Morbi blandit est nec sodales sagittis. Vivamus euismod egestas arcu, id mollis orci porttitor eleifend. Praesent leo enim, laoreet quis ullamcorper id, eleifend eu diam. Vestibulum sed lorem egestas, accumsan nunc vel, mollis orci.
+</p>
+
+<br>
+<br>
+
+        <div class="title-wrapper">
+            <h1 class="title">2018-2019 Families Theme: Blast to the Past</h1>
+	</div>
+
+
+
+
+<div class="contact-row">
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+                <p><strong>Koopa Troopa</strong></p>
+            </div>
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+                <p><strong>GAang GAang</strong></p>
+            </div>
+      </div>
+        <div class="contact-row">
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+                <p><strong>#TBT</strong></p>
+            </div>
+            <div>
+                <img src=" https://i.imgur.com/oOEaEqe.png   " />
+                <p><strong>Hundred Acre Hood</strong></p>
+            </div>
+           
+        </div>
+
+
+
+
+
+
+
+
+<div align="center">
+<br><br>
+<a class="button" href="">APPLY NOW!</a>
+<br><br><br></div>
+
+
+
+<div align="center">
+
+
+<iframe src="http://free.timeanddate.com/countdown/i6exmpj7/n137/cf100/cm0/cu4/ct0/cs0/ca0/co1/cr0/ss0/cac000/cpc000/pct/tcfff/fs200/szw448/szh189/tatFamily%20Reveal%20Countdown/tac000/tptTime%20since%20Event%20started%20in/tpc000/iso2018-11-14T00:00:00" allowTransparency="true" frameborder="0" width="448" height="189"></iframe>
+</div>
+
+
+
+<div class="title-wrapper">
+    <h1 class="title">Benefits:</h1>
+</div>
+
+
+<div class="contact-row">
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+
+            </div>
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+	    </div>
+            <div>
+                <img src="https://i.imgur.com/oOEaEqe.png" />
+            </div>
+ 
+
+
+
+      </div>
+ 
+
+
+
+
+</div>
+
+@endsection
+
