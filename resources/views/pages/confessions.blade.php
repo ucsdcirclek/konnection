7c807134d6538a5c25810271dd294cfc0562c61d
vkelkar1999 <vkelkar@ucsd.edu>
2020-01-29 19:49:27 -0800

    Fixed Iframe

--- resources/views/pages/confessions.blade.php
+++ resources/views/pages/confessions.blade.php
@@ -7,21 +7,33 @@
 @section('content')
     @include('layouts.header', array('headerTitle' => 'Confessions!'))
 
-    <style>
-     p.descr{
-         white-space: pre-line;
-     }
+  <!--   -->
+<style>
+  .myIframe {
+     position: relative;
+     padding-bottom: 65.25%;
+     padding-top: 30px;
+     height: 0;
+     overflow: auto; 
+     -webkit-overflow-scrolling:touch; //<<--- THIS IS THE KEY 
+     border: solid black 1px;
+} 
+.myIframe iframe {
+     position: absolute;
+     top: 0;
+     left: 0;
+     width: 100%;
+     height: 100%;
+}
+</style>
 
-     p.big{
-         line-height: 200%;
-     }
 
-    </style>
 
-    <div class="wrapper">
-        <div class="title-wrapper">
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd78-0NLUtVLGpGrqD_Aa03ysAaWIpRQJfRbmsMOduXhLPeJA/viewform" style="height:800px;width:1000px;"></iframe>
-        </div>
 
-    </div>
+   <div class="wrapper" > 
+     <div class="myIframe" > 
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd78-0NLUtVLGpGrqD_Aa03ysAaWIpRQJfRbmsMOduXhLPeJA/viewform" style= height:100%;width:100%;></iframe>
+      </div> 
+       <!-- style= height:800px;width:1000px; -->
+  </div> 
 @endsection
