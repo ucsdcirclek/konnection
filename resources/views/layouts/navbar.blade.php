11547b9d9841a559967ee11e3b1ca5b990c1bbe5
Samuel Liu <44206874+samliu000@users.noreply.github.com>
2020-07-06 22:26:34 -0700

    Change address (#239)
    * Composer.json commit -Sam
    
    * Square crop images
    
    * Removed warning text for profile pic selection
    
    * Merge fix
    
    * Changed URL's for family system and gallery
    
    Co-authored-by: vagrant <vagrant@homestead>

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -29,8 +29,8 @@
                     <ul class="nav-dropdown">
                         <li><a href="{{ url('about/circlek') }}">Circle K</a></li>
                         <li><a href="{{ url('about/club') }}">Club</a></li>
-                        <li><a href="{{ url('familysystem/landing') }}">Family System</a></li>
-                        <li><a href="{{ url('gallery') }}">Gallery</a></li>
+                        <li><a href="{{ url('about/familysystem') }}">Family System</a></li>
+                        <li><a href="{{ url('about/gallery') }}"">Gallery</a></li>
                         <li><a href="{{ url('about/district') }}">District</a></li>
                         <li><a href="{{ url('about/membership') }}">Membership</a></li>
                         <li><a target="_blank" href="http://www.kiwanis.org">Kiwanis</a></li>
